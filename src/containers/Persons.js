476fd2a22b87dcc8ab0c92fea23219e4100e0556
hyderdanyal <hyderdanyal@gmail.com>
2021-01-11 15:54:42 -0600

    assignmnet completed

---
+++ src/containers/Persons.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import  {connect} from 'react-redux'
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+
+class Persons extends Component {
+
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.props.personAddedHandler} />
+                {this.props.persons.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.personDeletedHandler(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state =>{
+    return{
+        persons:state.persons
+    }
+}
+const mapDispatchToProps = dispatch =>{
+    return {
+        personAddedHandler: ()=> dispatch({type: 'PERSONADDED'}),
+        personDeletedHandler: (id)=> dispatch({type: 'PERSONREMOVED', personId:id})
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Persons);
